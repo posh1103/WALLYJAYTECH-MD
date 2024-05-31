d3afd6e93fd7cb232bd786e40a231ed8ee605917
Ajibade Adewale Joseph <wallyjay123@gmail.com>
2024-05-31 03:55:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const bot = require(__dirname + '/lib/smd')
 const { VERSION } = require(__dirname + '/config')
 
 const start = async () => {
-    Debug.info(`Wallyjay ${VERSION}`)
+    Debug.info(`Wallyjaytech ${VERSION}`)
   try {
     await bot.init()
     bot.logger.info('⏳ Database syncing!')
